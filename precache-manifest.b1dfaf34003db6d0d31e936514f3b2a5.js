254458e38e539f32c67a169e8273db13f61afaa1
Guzel738 <66492070+Guzel738@users.noreply.github.com>
2020-11-10 20:25:26 +0300

    Updates

---
+++ precache-manifest.b1dfaf34003db6d0d31e936514f3b2a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5ade01e39e2920718c3089fcee1bec5",
+    "url": "/Quiz/index.html"
+  },
+  {
+    "revision": "a301c90ec3739c63b15b",
+    "url": "/Quiz/static/css/main.bee79c9e.chunk.css"
+  },
+  {
+    "revision": "c7fcf1cf4d96b658721d",
+    "url": "/Quiz/static/js/2.5f72fdb8.chunk.js"
+  },
+  {
+    "revision": "fae6e97322ddc0579046aa829b382727",
+    "url": "/Quiz/static/js/2.5f72fdb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a301c90ec3739c63b15b",
+    "url": "/Quiz/static/js/main.f31e7e19.chunk.js"
+  },
+  {
+    "revision": "e882df350f55914a2ef9",
+    "url": "/Quiz/static/js/runtime-main.bf65ed40.js"
+  }
+]);
